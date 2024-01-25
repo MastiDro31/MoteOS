480caf00507b8a29044c9fceab92ca2cb04738e2
MastiDro31 <60008318+MastiDro31@users.noreply.github.com>
2024-01-25 18:14:38 +0600

    create main.lua

--- Installer/main.lua
+++ Installer/main.lua
@@ -1,1 +1,70 @@
+local function getCompAdr(name)
+	return component.list(name)() or error("Required " .. name .. " component is missing")
+end
 
+local EEPROMAdr, internetAdr, GPUAdr = 
+	getComponentAddress("eeprom"),
+	getComponentAddress("internet"),
+	getComponentAddress("gpu")
+
+component.invoke(GPUAddress, "bind", getComponentAddress("screen"))
+local screenWidth, screenHeight = component.invoke(GPUAddress, "getResolution")
+
+local function rawRequest(url, chunkHandler)
+	local internetHandle, reason = component.invoke(internetAddress, "request", repositoryURL .. url:gsub("([^%w%-%_%.%~])", function(char)
+		return string.format("%%%02X", string.byte(char))
+	end))
+
+	if internetHandle then
+		local chunk, reason
+		while true do
+			chunk, reason = internetHandle.read(math.huge)	
+			
+			if chunk then
+				chunkHandler(chunk)
+			else
+				if reason then
+					error("Internet request failed: " .. tostring(reason))
+				end
+
+				break
+			end
+		end
+
+		internetHandle.close()
+	else
+		error("Connection failed: " .. url)
+	end
+end
+
+local function request(url)
+	local data = ""
+	
+	rawRequest(url, function(chunk)
+		data = data .. chunk
+	end)
+
+	return data
+end
+
+local function download(url, path)
+	selectedFilesystemProxy.makeDirectory(filesystemPath(path))
+
+	local fileHandle, reason = selectedFilesystemProxy.open(path, "wb")
+	if fileHandle then	
+		rawRequest(url, function(chunk)
+			selectedFilesystemProxy.write(fileHandle, chunk)
+		end)
+
+		selectedFilesystemProxy.close(fileHandle)
+	else
+		error("File opening failed: " .. tostring(reason))
+	end
+end
+
+local files = {
+}
+
+for i = 1, #files.installerFiles do
+	download(files.installerFiles[i], installerPath .. files.installerFiles[i])
+end
